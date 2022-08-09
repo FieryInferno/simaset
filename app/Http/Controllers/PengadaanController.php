e451971efa3c4b671b5c4a7d46475a8981f183d9
FieryInferno <bagassetia271@gmail.com>
2022-08-09 11:54:17 +0700

    delete pengadaan

--- app/Http/Controllers/PengadaanController.php
+++ app/Http/Controllers/PengadaanController.php
@@ -91,40 +91,12 @@ class PengadaanController extends Controller
     return redirect('pengadaan_aset')->with('success', 'Berhasil tambah aset.');
   }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  public function destroy($id)
+  {
+    $aset = Aset::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    $aset->delete();
+    
+    return redirect('pengadaan_aset')->with('success', 'Berhasil hapus pengajuan.');
+  }
 }
